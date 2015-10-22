98dcd075017200252ff0068687dc78a4034bdeaa
Priscila <priscilatrespach@gmail.com>
2015-10-22 21:27:12 -0200

    implementado as classes AlunoServletJMS, Aluno, AlunoQueueMDB; No Wildlfy copiado o endereço java:jboss/exported/jms/queue/alunoQueue e colado na classe AlunoServletJMS(Resource(mappedName = "java:jboss/exported/jms/queue/alunoQueue")

---
+++ ExJMS/src/main/java/br/univel/jms/Aluno.java
@@ -0,0 +1,26 @@
+package br.univel.jms;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+	
+	private long Matricula;
+	private String Nome;
+	
+	
+	public long getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(long matricula) {
+		Matricula = matricula;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	
+
+}
