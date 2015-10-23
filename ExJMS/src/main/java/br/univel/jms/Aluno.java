d4d3d5d22704429a36ec6ae1adb3209b605e7e2a
Priscila <priscilatrespach@gmail.com>
2015-10-22 22:22:30 -0200

    implementado as classes Aluno, AlunoqueueMDB, AlunoServletJMS, Professor, ProfessorServletJMS, ProfessorTopicMDB1, ProfessorTopicMDB2, ProfessorTopicMDB3
    alterado 
    @Resource(mappedName = "java:jboss/exported/jms/topic/professorTopic")

---
+++ ExJMS/src/main/java/br/univel/jms/Aluno.java
@@ -0,0 +1,32 @@
+package br.univel.jms;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+	
+	private String nome;
+	private long   matricula;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", matricula=" + matricula + "]";
+	}
+	
+	
+
+	
+	
+}
