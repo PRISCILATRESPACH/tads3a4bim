58c8929ffcbc32cb6e6b348ff1d6f678660c6f5b
Priscila <priscilatrespach@gmail.com>
2015-10-28 20:37:02 -0200

    implementado as classes ItemProcessor, DefaultItemDAO

--- ExCDI/src/main/java/br/univel/cdi/ItemProcessor.java
+++ ExCDI/src/main/java/br/univel/cdi/ItemProcessor.java
@@ -6,19 +6,19 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-
-
 @Named
 @RequestScoped
 public class ItemProcessor {
-
-    @Inject
+	
+	@Inject
 	private ItemDao itemDao;
-
-    public void execute() {
+	
+	public void execute() {
         List<Item> items = itemDao.fetchItems();
         for (Item item : items) {
             System.out.println("Found item " + item);
+            
         }
-    }
+	}
+
 }
